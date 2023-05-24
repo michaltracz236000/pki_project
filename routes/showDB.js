b01d80fefeaf72706f5dfdd95b348f962243d1c1
michaltracz <michaltracz@gmail.com>
2023-05-24 16:50:42 +0200

    show query in main page

--- routes/showDB.js
+++ routes/showDB.js
@@ -2,37 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-    if(req.body.db!= undefined)
-    {
-        text=''
-  const query = {
-    text: 'SELECT * FROM '+req.body.db,
-  };
-  pool.query(query)
-  .then((result) => {
-    console.log(result.rows);
-    res.send(result.rows);
-  })
-  .catch((error) => {
-    res.send("NIE dostałem1");
-  });
+router.post('/', function (req, res, next) {
+    if (req.body.db != undefined) {
+        text = ''
+        const query = {
+            text: 'SELECT * FROM ' + req.body.db,
+        };
+        pool.query(query)
+            .then((result) => {
+                console.log(result.rows);
+                text = ""
+                text += result.rows + '<form method="GET" action="/"><button type="submit">Powrót do strony głównej</button></form>'
+                res.send(text);
+            })
+            .catch((error) => {
+                res.send("NIE dostałem1");
+            });
     }
-    else if(req.body.query!=undefined)
-    {
-        console.log(req.body.query);
+    else if (req.body.query != undefined) {
         pool.query(req.body.query)
-  .then((result) => {
-    
-    console.log(result);
-    res.send(result);
-  })
-  .catch((error) => {
-    res.send(error);
-  });
+            .then((result) => {
+                console.log(req.body.query);
+                text = ""
+                text += result.rows + '<form method="GET" action="/"><button type="submit" name="query" value="' + req.body.query + '">Powrót do strony głównej</button></form>'
+                console.log(result);
+                res.send(text);
+            })
+            .catch((error) => {
+                text = ""
+                text += error + '<form method="GET" action="/"><button type="submit" name="query" value="' + req.body.query + '">Powrót do strony głównej</button></form>'
+                res.send(text);
+            });
     }
-    else
-    {
+    else {
         res.send("BRAK DANYCH");
     }
 });
